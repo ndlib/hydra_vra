a804cccbb8cb34dc68760aeabb9b6f74f71d928e
Banurekha L <blakshmi@nd.edu>
2010-10-20 14:20:08 -0400

    New file for agent partial and jquery for vra metadata

---
+++ public/javascripts/jquery.VraMetadata.js
@@ -0,0 +1,34 @@
+  (function($) {
+   /*  Initialize the form for inserting new Person (individual) permissions
+   *  ex. $("#add-contributor-box").hydraNewContributorForm
+   */
+   $.fn.hydraNewAgentForm = function(settings) {
+     var config = {};
+
+     if (settings) $.extend(config, settings);
+
+     this.each(function() {
+       $("#re-run-add-agent-action", this).click(function() {
+         addAgent("agent");
+       });
+     });
+
+     return this;
+
+   };
+
+     addAgent: function(type) {
+       var content_type = $("form#new_contributor > input#content_type").first().attr("value");
+       var contributors_group_selector = "."+type+".contributor";
+
+       var url = $("form#new_agent").attr("action");
+
+       $.post(url, {contributor_type: type, content_type: content_type},function(data) {
+         $(contributors_group_selector).last().after(data);
+         $inserted = $(contributors_group_selector).last()
+         $(".editable-container", $inserted).hydraTextField();
+         $("a.destructive", $inserted).hydraContributorDeleteButton();
+       });
+     }
+
+ })(jQuery);
