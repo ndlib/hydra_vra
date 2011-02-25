0f8143b4a65a6a9b5c40623df1f38a20cea8dba5
Banurekha L <blakshmi@nd.edu>
2011-02-25 12:57:01 -0500

    some changes to deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,62 +1,124 @@
-# This is an example capistrano recipe for deploying blacklight. 
-# We use it to deploy the application at demo.blacklightopac.org
-# Your milage may vary for local usage. 
+#set all tasks from RAILS_ROOT using: cap -T
 
-set :application, "bl-demo"
+#############################################################
+#  Application
+#############################################################
 
-# name of the user who will own this application on the server 
-set :user, "deployer"
-# SVN repository from which to check out the code
-set :repository,  "svn+ssh://eos8d@rubyforge.org/var/svn/blacklight/trunk/rails"
+set :application, 'currency'
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
- set :deploy_to, "/usr/local/projects/#{application}"
+#############################################################
+#  Settings
+#############################################################
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+default_run_options[:pty] = true
+set :use_sudo, false
 
-# If you run your app, your webserver (httpd) and/or your database on different servers, you can 
-# set each of these to unique values
-set :domain, "polaris.lib.virginia.edu"
-role :app, domain
-role :web, domain
-role :db,  domain, :primary => true
+#############################################################
+#  Github
+#############################################################
 
-set :runner, "deployer"
-set :rails_env, "production"
+set :repository, "git@github.com:ndliblis/hydra_exhibit.git"  
+    #"rails@ambrosiana.library.nd.edu:/home/rails/github/#{application}/.git"
+set :scm, :git
+set :branch, "master"
+set :deploy_via, :remote_cache
 
-# This assumes that your database.yml file is NOT in subversion,
-# but instead is in your deploy_to/shared directory. Database.yml
-# files should *never* go into subversion for security reasons.
-task :after_update_code do
-  run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
-  run "ln -nfs #{deploy_to}/shared/config/solr.yml #{release_path}/config/solr.yml"
+#############################################################
+#  Environments
+#############################################################
+
+desc "Setup for the Staging environment"
+task :staging do
+  set :rails_env,     'staging'
+  set :rake,          '/shared/ruby/bin/rake'
+  set :bundler,       '/shared/ruby/bin/bundle'
+  set :deploy_to,     "/data/web_root/htdocs/rails_apps/#{application}"
+  set :user,          'rails'
+  set :domain,        'ambrosiana.library.nd.edu'
+  set :site_url,      'currencytaging.library.nd.edu'
+  set :app_server,    'passenger'
+
+  server "#{user}@#{domain}", :app, :web, :db, :primary => true
 end
 
-# ========================
-# For mod_rails apps
-# ========================
+desc "Setup for the Pre-Production environment"
+task :pre_production do
+  set :rails_env,      'pre_production'
+  # NOTE the local svn command must also be available at the remote svn command path, a symlink may be necessary
+  #set :scm_command,    '/shared/svn/binaries/bin/svn'
+  set :rake,           '/shared/ruby187/bin/rake'
+  set :bundler,        '/shared/ruby187/bin/bundle'
+  set :deploy_to,      "/shared/ruby_server_pprd/data/app_home/#{application}"
+  set :user,           'rubypprd'
+  set :domain,         'rbpprd.library.nd.edu'
+  set :site_url,       'currencypprd.library.nd.edu'
+  set :app_server,     'thin'
+
+  server "#{user}@#{domain}", :app, :web, :db, :primary => true
+end
+
+#############################################################
+#  Deploy
+#############################################################
 
 namespace :deploy do
+  desc "Start application"
   task :start, :roles => :app do
-    run "touch #{deploy_to}/current/tmp/restart.txt"
+    case app_server
+    when "passenger"
+      run "touch #{current_path}/tmp/restart.txt"
+    when "thin"
+      run "/shared/ruby_server_pprd/admin/start_stop_hydrangea.sh start"
+    end
   end
-  
+
+  desc "Restart application"
   task :restart, :roles => :app do
-    run "touch #{deploy_to}/current/tmp/restart.txt"
+    case app_server
+    when "passenger"
+      run "touch #{current_path}/tmp/restart.txt"
+    when "thin"
+      run "/shared/ruby_server_pprd/admin/start_stop_hydrangea.sh restart"
+    end
   end
-  
-  task :after_symlink, :roles => :app do
-    run "cp #{deploy_to}/current/vendor/plugins/blacklight/config/initializers/blacklight_config.rb #{deploy_to}/current/config/initializers/blacklight_config.rb"
-    # this next step shouldn't be necessary for rails 2.3, and yet the installation on polaris.lib won't 
-    # run without it
-    run "ln -nfs #{deploy_to}/current/vendor/plugins/blacklight/app/controllers/application_controller.rb #{deploy_to}/current/vendor/plugins/blacklight/app/controllers/application.rb"
-    
-    # reindex the data
-    run "cd #{deploy_to}/current; rake solr:marc:index MARC_FILE=/usr/local/projects/data/test_data.utf8.mrc SOLR_WAR_PATH=/usr/local/projects/solr/solr.war CONFIG_PATH=#{deploy_to}/current/vendor/plugins/blacklight/config/SolrMarc/demoserver.properties"
-    run "cd #{deploy_to}/current; rake app:index:ead_dir FILE=/usr/local/projects/data/ead/*.xml"
+
+  desc "Stop application"
+  task :stop, :roles => :app do
+    case app_server
+    when "thin"
+      run "/shared/ruby_server_pprd/admin/start_stop_hydrangea.sh stop"
+    end
+  end
+
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared, :roles => :app do
+    run "ln -nfs #{shared_path}/log #{release_path}/log"
+
+    # Bundle management: when deployed use frozen gems
+    # NOTE Update gems on the server with: bundle install --deployment
+    run "ln -nfs #{shared_path}/bundle/config #{release_path}/.bundle/config"
+    run "ln -nfs #{shared_path}/vendor/bundle #{release_path}/vendor/bundle"
+
+    # NOTE configuration files should not be kept in source control for security reasons.
+    ['database', 'solr', 'fedora'].each do |file|
+      run "ln -nfs #{shared_path}/config/#{file}.yml #{release_path}/config/#{file}.yml"
+    end
+
+    # NOTE after the transition to mysql from sqlite remove the following line:
+    run "ln -nfs #{shared_path}/db/#{rails_env}.sqlite3 #{release_path}/db/#{rails_env}.sqlite3"
+  end
+
+  desc "Install gems in Gemfile"
+  task :bundle_install, :roles => :app do
+    run "cd #{release_path} && #{bundler} install"
+  end
+
+  desc "Spool up Passenger spawner to keep user experience speedy"
+  task :kickstart, :roles => :app do
+    run "curl -I http://#{site_url}"
   end
 end
+
+after 'deploy:update_code', 'deploy:symlink_shared', 'deploy:bundle_install', 'deploy:migrate'
+after 'deploy', 'deploy:cleanup'
+
