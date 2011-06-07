f4843038e903035d812c9e6572b8beb28043b591
Banurekha L <blakshmi@nd.edu>
2011-06-07 09:39:01 -0400

    ldap config example

--- config/initializers/ldap_config_example.rb
+++ config/initializers/ldap_config_example.rb
@@ -1,21 +1,25 @@
+# NOTE change these settings for your institution and rename the file to ldap_config.rb
+# If you wish to add ldap_config.rb to source control edit the .gitingore file
+
 # connection settings
-LDAP_HOST = 'directory.nd.edu'
-LDAP_BASE = 'o=University of Notre Dame,st=Indiana,c=US'
+LDAP_HOST = "ldap.virginia.edu"
+LDAP_BASE = "o=University of Virginia,c=US"
 LDAP_PORT = 389
 
 LDAP_ACCESS_METHOD   = :simple
-LDAP_ACCESS_USER     = ''
-LDAP_ACCESS_PASSWORD = 'anonymous'
+LDAP_ACCESS_USER     = ""
+LDAP_ACCESS_PASSWORD = ""
 
 # the name of your institution
-LDAP_INSTITUTION = 'University of Notre Dame'
+LDAP_INSTITUTION = ""
 
 # specific ldap field names
-LDAP_USER_ID        = 'uid'
-LDAP_FIRST_NAME     = 'givenname'
-LDAP_LAST_NAME      = 'sn'
-LDAP_NICKNAME       = 'ndvanityname'
-LDAP_AGGREGATE_NAME = 'cn'
-LDAP_PREFERRED_NAME = 'ndvanityname'
-LDAP_DEPARTMENT     = 'ndprimaryaffiliation'
-LDAP_PHOTO          = ''
+LDAP_USER_ID        = "userid"
+LDAP_FIRST_NAME     = "givenname"
+LDAP_LAST_NAME      = "sn"
+LDAP_AGGREGATE_NAME = ""
+LDAP_PREFERRED_NAME = ""
+LDAP_COMPUTING_ID   = "uid"
+LDAP_DEPARTMENT     = "department"
+LDAP_PHOTO          = "jpegphoto"
+
